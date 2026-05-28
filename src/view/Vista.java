bc8331d09c0dab5bdf06f01dcaa47de1542334c4
Andrés Cecconi <cecconiandres.dev@gmail.com>
2026-05-28 20:50:58 -0300

    refactor: renombrar Interfaz_test a Vista y limpiar contenido inicial

---
+++ src/view/Vista.java
@@ -0,0 +1,11 @@
+package view;
+
+import javax.swing.JFrame;
+
+/**
+ * Ventana principal del juego. Maneja la interfaz gráfica,
+ * el game loop y los eventos de teclado del jugador.
+ */
+public class Vista extends JFrame {
+
+}
