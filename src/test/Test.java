61513e08c00ef5ec4b9365f411c06947c5a8488b
Andrés Cecconi <cecconiandres.dev@gmail.com>
2026-05-28 20:51:26 -0300

    feat: agregar clase Test como unico punto de entrada de la aplicacion

---
+++ src/test/Test.java
@@ -0,0 +1,14 @@
+package test;
+
+import view.Vista;
+
+/**
+ * Punto de entrada de la aplicación.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+        new Vista();
+    }
+
+}
